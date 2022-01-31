2945cc1d93ac77b92f1e38af98b2d58715d7fb59
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2022-01-31 16:11:32 +0100

    Added test to update sensor data

--- GUI_loop.py
+++ GUI_loop.py
@@ -8,14 +8,21 @@ from PyQt5.QtWidgets import QMainWindow
 
 
 class Window(QMainWindow, SUBSEAGUI.Ui_MainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, conn, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        
+        threading.Thread(target=lambda: self.recieve_and_set_text(conn)).start()
 
+    def recieve_and_set_text(self, conn):
+        print("trying to take out of pipe")
+        while True:
+            data = conn.recv()
+            self.dybde.setText(str(data))
 
-def run(conn = None):
+def run(conn):
     app = QtWidgets.QApplication(sys.argv)
-    win = Window()
+    win = Window(conn)
     win.show()
     sys.exit(app.exec())
     SUBSEAGUI.MainWindow = QtWidgets.QMainWindow()
@@ -37,7 +44,7 @@ def generate_data(conn):
     while True:
         time.sleep(0.5)
         print("tring to send on pipe")
-        conn.send(bytes(random.randrange(1,10)))
+        conn.send((random.randrange(65,97)))
 
 if __name__ == "__main__":
     send_to_GUI, receive_from_GUI = Pipe()
